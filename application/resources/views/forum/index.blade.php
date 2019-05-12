d56d436acae15b870f4724a1d060b16cc06f1ce0
SuuPoo <main@suu-poo.com>
2019-05-12 14:58:29 +0900

    新規作成ボタンの追加

--- application/resources/views/forum/index.blade.php
+++ application/resources/views/forum/index.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 <div class="row">
     <div class="col">
+        <a href="{{route('forum.create')}}" class="btn btn-primary btn-block">
+            新規作成
+        </a>
+    </div>
+</div>
+<hr class="my-2">
+<div class="row">
+    <div class="col">
         <ul class="list-group list-group-flush">
             @foreach($forums as $forum)
             <li class="list-group-item">
