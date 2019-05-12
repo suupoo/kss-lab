055d73b58c5cf877eaf1e140c1eab22da06789bf
SuuPoo <main@suu-poo.com>
2019-05-12 13:13:49 +0900

    テンプレートの追加

---
+++ application/resources/views/forum/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col">
+        <h2>スレッド作成</h2>
+        <form id="fbody1" action="{{route('forum.store')}}" method="POST">
+            {{csrf_field()}}
+            <div class="form-group" data-form="f1">
+                @include('components.forms.input',[
+                    'id'=>'title','title'=>'タイトル','type'=>'text','class'=>'','placeholder'=>'','expText'=>'50文字以内で入力してください。',
+                    'max'=>'','min'=>'','maxlength'=>'50','minlength'=>'1',
+                    'value'=>'',
+                    'required'=>true,'readonly'=>false,
+                ])
+            </div>
+            <div class="form-group" data-form="f2">
+                @include('components.forms.select',[
+                    'id'=>'category','title'=>'category','expText'=>'カテゴリを選択してください。',
+                    'multiple'=>true,
+                    'options'=>[
+                        'category1'=>'テストカテゴリ1',
+                        'category2'=>'テストカテゴリ2'
+                    ],
+                ])
+            </div>
+            <div class="form-group" data-form="f3">
+                @include('components.forms.textarea',[
+                    'id'=>'content','title'=>'内容','class'=>'','expText'=>'内容を500文字以内で入力してください。',
+                    'maxlength'=>'500','minlength'=>'1',
+                    'value'=>'',
+                    'rows'=>'5','cols'=>'5',
+                    'required'=>true,'readonly'=>false,
+                ])
+            </div>
+            <div class="form-group" data-form="f4">
+                @include('components.forms.select',[
+                    'id'=>'status','title'=>'category','expText'=>'公開ステータスを選択してください。',
+                    'options'=>\App\Http\Models\Forum::getOptionForStatus(),
+                ])
+            </div>
+            @include('components.forms.btn-submit',['btnType'=> 'btn-primary','class'=>'btn-block','title'=>'送信'])
+        </form>
+    </div>
+</div>
+@endsection
---
+++ application/resources/views/forum/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-sm">
+        One of three columns
+    </div>
+    <div class="col-sm">
+        One of three columns
+    </div>
+    <div class="col-sm">
+        One of three columns
+    </div>
+</div>
+@endsection
