227966fb590b42f0f13c43091ca99becf7c3fc67
SuuPoo <main@suu-poo.com>
2019-05-14 21:19:24 +0900

    Commentモデルとマイグレーション

---
+++ application/database/migrations/2019_05_14_195402_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('comment',500);
+            $table->tinyInteger('type');
+            //固定
+            $table  ->bigInteger('edit_user');
+            $table  ->smallInteger('status');
+            $table  ->boolean('visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
