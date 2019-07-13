126dad81306947ff03541fb5e4797662956f6a63
SuuPoo <main@suu-poo.com>
2019-07-13 10:48:42 +0900

    フォーラムから個人ページへ行く際のフォームが不要のため削除

--- application/resources/views/forum/show.blade.php
+++ application/resources/views/forum/show.blade.php
@@ -64,15 +64,13 @@
         </form>
     </div>
     <div class="row">
-        <form action="{{route('account.show',['public_id'=>$forum->id])}}">
-            <div class="card" style="width: 18rem;">
-                <ul class="list-group list-group-flush">
-                @foreach($forum->comments as $comment)
-                    @component('components.comment.comment',['comment'=>$comment,'user'=>$commentUsers[$comment->user_id]])@endcomponent
-                @endforeach
-                </ul>
-            </div>
-        </form>
+        <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+            @foreach($forum->comments as $comment)
+                @component('components.comment.comment',['comment'=>$comment,'user'=>$commentUsers[$comment->user_id]])@endcomponent
+            @endforeach
+            </ul>
+        </div>
     </div>
 </div>
 <div class="row" id="files">
