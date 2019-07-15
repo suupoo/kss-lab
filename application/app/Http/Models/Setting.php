171c3ea6338ce13894a778c59410a56aa71e5028
SuuPoo <main@suu-poo.com>
2019-07-15 21:45:23 +0900

    Settingモデルのテーブル参照先がconfigsになっていた不具合を修正

--- application/app/Http/Models/Setting.php
+++ application/app/Http/Models/Setting.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    protected $table = 'configs';
+    protected $table = 'settings';
     protected $primaryKey = 'id';
     protected $dates = ['deleted_at'];
 
