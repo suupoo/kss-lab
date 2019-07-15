8641821930c9c3bad8bda58a02cba2db9346d936
SuuPoo <main@suu-poo.com>
2019-07-15 22:57:50 +0900

    Settingファサードへユーザの個別設定値を取得するgetメソッドを実装

--- application/app/Facades/Components/SettingComponent.php
+++ application/app/Facades/Components/SettingComponent.php
@@ -3,18 +3,21 @@ namespace App\Facades\Components;
 
 use App\Http\Models\User;
 use App\Http\Models\Setting;
-use App\Http\Models\Config;
 
 class SettingComponent
 {
     /**
-     * 設定値を取得します。
+     * ユーザの設定値を取得します。
      *
      * @param User $user
-     * @param array $names
+     * @return Setting[]|bool|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
      */
-    public function get(User $user, array $names = [])
+    public function get(User $user)
     {
-        return true;
+        $settings = Setting::with('config')
+            ->where(Setting::USER_ID , $user->id)
+            ->get();
+
+        return $settings;
     }
 }
