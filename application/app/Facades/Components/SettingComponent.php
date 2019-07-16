37690638ea88f83c125343a83c0646bf5c5f1cae
SuuPoo <main@suu-poo.com>
2019-07-16 21:06:16 +0900

    設定値ファサードのgetメソッドに設定値名でフィルタリングするパラメータを追加

--- application/app/Facades/Components/SettingComponent.php
+++ application/app/Facades/Components/SettingComponent.php
@@ -1,23 +1,36 @@
 <?php
 namespace App\Facades\Components;
 
+use App\Http\Models\Config;
 use App\Http\Models\User;
 use App\Http\Models\Setting;
+use function foo\func;
+use Illuminate\Database\Eloquent\Collection;
 
 class SettingComponent
 {
     /**
      * ユーザの設定値を取得します。
      *
-     * @param User $user
+     * @param User $user    取得するユーザ
+     * @param array $names  設定値名称の配列
+     *
      * @return Setting[]|bool|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
      */
-    public function get(User $user)
+    public function get(User $user, array $names = [])
     {
         $settings = Setting::with('config')
             ->where(Setting::USER_ID , $user->id)
             ->get();
 
+        if($names){
+            $settings = $settings->filter(function ($value, $key) use ($names){
+                // パラメータが指定されている場合
+                if( in_array($value->config->name, $names) )
+                    return true;
+            })
+            ->all();
+        }
         return $settings;
     }
 }
