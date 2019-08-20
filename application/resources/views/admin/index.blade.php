0a705a3f6e5ea22ffbda9f01d85937f9abfc0ce7
SuuPoo <main@suu-poo.com>
2019-08-20 23:26:21 +0900

    ユーザ管理機能の基礎を追加

---
+++ application/resources/views/admin/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col">
+
+    </div>
+</div>
+<hr class="my-2">
+<div class="row">
+    <div class="col">
+        <ul class="list-group list-group-flush">
+
+        </ul>
+    </div>
+</div>
+@endsection
