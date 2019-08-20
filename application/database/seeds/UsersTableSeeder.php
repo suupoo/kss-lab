4abb9dd17c3bc5f19ca3ad83bee6fd8d7393a980
SuuPoo <main@suu-poo.com>
2019-08-20 23:27:13 +0900

    ユーザ情報シードの修正

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -24,7 +24,7 @@ class UsersTableSeeder extends Seeder
             'phone_number'
                         =>  null,
             'email'     =>  env('TEST_ADMIN_MAIL','admin@kss.lab'),
-            'password'  =>  '$2y$10$JPj5ATn1OPKmWS.8lmWPjOLDyGlqKkrYA/GBmOCtVKVnoA91PbG6W',
+            'password'  =>  '$2y$10$4kyHU7ZR.kqqQitIXab9COU7gEhrJllqERyrj88G2r6O3/pvZ1.nu',
             'created_at'=>new DateTime('2019/06/11 00:00:00'),
             'updated_at'=>new DateTime('2019/06/11 00:00:00'),
         ]);
@@ -40,7 +40,7 @@ class UsersTableSeeder extends Seeder
             'phone_number'
                         =>  env('SMS_FROM'),
             'email'     =>  env('TEST_USER1_MAIL','test1@kss.lab'),
-            'password'  =>  '$2y$10$u3SVvoMdIXNR5j7VNZluEuTC1tbYrE/Rxyv.DJ9MSpCQpo.OCl6Em',
+            'password'  =>  '$2y$10$ArlpFXQZTENmnbiKgqbPde9JhNZP//jcKyOL3K3QfpSSb38jpQLEK',
             'created_at'=>new DateTime('2019/05/11 00:00:00'),
             'updated_at'=>new DateTime('2019/05/11 00:00:00'),
         ]);
@@ -56,7 +56,7 @@ class UsersTableSeeder extends Seeder
             'phone_number'
                         =>  null,
             'email'     =>  env('TEST_USER2_MAIL','test2@kss.lab'),
-            'password'  =>  '$2y$10$JPj5ATn1OPKmWS.8lmWPjOLDyGlqKkrYA/GBmOCtVKVnoA91PbG6W',
+            'password'  =>  '$2y$10$n8YisKhWSvrFTl4Fb3ly9unQOrzQdW6pKXVCF6ZFvZZ4xYIq8cSty',
             'created_at'=>new DateTime('2019/06/11 00:00:00'),
             'updated_at'=>new DateTime('2019/06/11 00:00:00'),
         ]);
