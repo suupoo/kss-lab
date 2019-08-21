763f92d9007ff523f782a4b2d32a0592c6d24602
SuuPoo <main@suu-poo.com>
2019-08-21 22:13:24 +0900

    ビューの修正

---
+++ application/resources/views/admin/user/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col">
+        <h1>ユーザ管理</h1>
+    </div>
+</div>
+<hr class="my-2">
+<div class="row">
+    <div class="col">
+        <ul class="list-group list-group-flush">
+
+        </ul>
+    </div>
+</div>
+@endsection
