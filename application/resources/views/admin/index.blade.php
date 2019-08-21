4af566c4362c184af4ebec9da105931963fb66eb
SuuPoo <main@suu-poo.com>
2019-08-21 22:14:27 +0900

    管理機能トップのビュー修正

--- application/resources/views/admin/index.blade.php
+++ application/resources/views/admin/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="row">
     <div class="col">
-
+        <h1 class="">管理</h1>
     </div>
 </div>
 <hr class="my-2">
 <div class="row">
     <div class="col">
         <ul class="list-group list-group-flush">
-
+            <a class="nav-link" href="{{ route('admin.user') }}">ユーザ</a>
         </ul>
     </div>
 </div>
